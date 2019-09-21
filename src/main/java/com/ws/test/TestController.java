ff48bc852bbb20ff792843a4d5ae91e6779f969d
wangduoyuan <wangduoyuan@kingsoft.com>
2019-09-21 15:57:49 +0800

    优化代码

--- src/main/java/com/ws/test/TestController.java
+++ src/main/java/com/ws/test/TestController.java
@@ -1,12 +1,9 @@
 package com.ws.test;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 
 
 /**
@@ -18,21 +15,16 @@ import javax.annotation.Resource;
 @RequestMapping(value = "/test")
 public class TestController {
 
-//    @Resource
-//    RestTemplate restTemplate;
-    @Resource
-    TestService testService;
     /**
-     * @Description: 获取名称
-     * @return:
-     * @author:wangdy
-     * @Date: 2019/8/22 下午8:58
-    */
-    @GetMapping("/randoms")
-    public String getRandoms(){
-        //String names = restTemplate.getForObject("http://eureka-client-2/eureka2/test/names",String.class);
-        String random = testService.getRandom();
-        return random;
+     * @description:
+     * @param
+     * @return: 
+     * @author: 
+     * @Date: 2019/9/19 下午3:00
+    */ 
+    @GetMapping("/hi")
+    public String hi(){
+        return "eureka-client-1:8811";
     }
 
 
